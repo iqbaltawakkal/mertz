fa8a9474e52ee9dd3dd78755bb44adda5b0629c2
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2020-10-03 09:59:58 +0700

    dummy carousel

---
+++ components/Carousel.vue
@@ -0,0 +1,40 @@
+<template>
+  <hooper ref="carousel" style="height: auto" :settings="hooperSettings">
+    <slide v-for="i in 4" :key="i">
+      <div>
+        <img :src="`./carousel/${i}.jpg`" />
+      </div>
+    </slide>
+    <pagination slot="hooper-addons"></pagination>
+  </hooper>
+</template>
+
+<script>
+import { Hooper, Slide, Pagination } from 'hooper'
+import 'hooper/dist/hooper.css'
+
+export default {
+  name: 'Div',
+  components: {
+    Hooper,
+    Slide,
+    Pagination,
+  },
+  data() {
+    return {
+      hooperSettings: {
+        itemsToShow: 1,
+        itemsToSlide: 1,
+        infiniteScroll: 1,
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+li:focus,
+button:focus {
+  outline: none;
+}
+</style>
