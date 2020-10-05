395bd787eb4626601e34feb2e994b9bd7b9d13cf
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2020-10-05 17:49:34 +0700

    header updt

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="bg-white w-full">
+    <div class="flex py-3 justify-between items-center">
+      <span
+        class="antialiased font-comfortaa text-3xl text-gray-900 hover:opacity-50 cursor-pointer"
+        >mértz
+      </span>
+      <div>
+        <a
+          href="#"
+          class="text-sm px-3 py-2 leading-none border rounded text-gray-900 border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-600 mt-4 lg:mt-0"
+          >Shop Now</a
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toggle: false,
+    }
+  },
+}
+</script>
+
+<style>
+.font-comfortaa {
+  font-family: 'Comfortaa', cursive;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="min-h-screen bg-gray-200">
-    <div class="max-w-lg mx-auto w-full px-3 bg-white min-h-screen"></div>
+    <div class="max-w-lg mx-auto w-full px-3 bg-white min-h-screen">
+      <Header />
+      <Carousel :is-number-page="false" />
+      <h1 class="font-sans text-xl">Category</h1>
+    </div>
   </section>
 </template>
 
